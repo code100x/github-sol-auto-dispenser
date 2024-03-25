5cb0c9219a93a4e0a84735c7244582f565722f97
TANMAY <tanmay@TANMAYs-MacBook-Air.local>
2024-03-26 02:17:13 +0530

    basic next app with sdhadcn and turbo

---
+++ my-turborepo/apps/100xbounty-bot/README.md
@@ -0,0 +1,35 @@
+## WIP
+
+# bounty-bot
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that A bot that dispatches event based on pr messages
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Docker
+
+```sh
+# 1. Build container
+docker build -t bounty-bot .
+
+# 2. Start container
+docker run -e APP_ID=<app-id> -e PRIVATE_KEY=<pem-value> bounty-bot
+```
+
+## Contributing
+
+If you have suggestions for how bounty-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2024 devsargam
---
+++ my-turborepo/apps/100xbounty-bot/src/discord.ts
@@ -0,0 +1,66 @@
+import { EmbedBuilder, WebhookClient } from "discord.js";
+
+export const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL! });
+
+type sendBountyTypes = {
+  title: string;
+  description: string;
+  avatarUrl: string;
+  prLink: string;
+}
+export const sendBountyMessageToDiscord = async ({ title, description, avatarUrl, prLink }: sendBountyTypes) => {
+  const embed = new EmbedBuilder()
+    .setTitle(title)
+    .setDescription(description)
+    .setThumbnail(avatarUrl)
+    .addFields({
+      name: "PR",
+      value: prLink,
+    })
+
+  await webhookClient.send({
+    username: 'Github',
+    avatarURL:
+      "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+    embeds: [embed],
+  });
+}
+
+type sendPrTypes = {
+  title: string;
+  avatarUrl: string;
+  prLink: string;
+}
+export const sendPrOpenToDiscord = async ({ title,  avatarUrl, prLink }: sendPrTypes) => {
+  const embed = new EmbedBuilder()
+    .setTitle(title)
+    .setDescription(prLink)
+    .setThumbnail(avatarUrl)
+
+  await webhookClient.send({
+    username: 'Github',
+    avatarURL:
+      "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+    embeds: [embed],
+  });
+}
+
+
+type SendIssueType = {
+  title: string;
+  avatarUrl: string;
+  issueLink: string;
+}
+export const sendIssueOpenToDiscord = async ({ title,  avatarUrl, issueLink }: SendIssueType) => {
+  const embed = new EmbedBuilder()
+    .setTitle(title)
+    .setDescription(issueLink)
+    .setThumbnail(avatarUrl)
+
+  await webhookClient.send({
+    username: 'Github',
+    avatarURL:
+      "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+    embeds: [embed],
+  });
+}
