77ab7c3ae24ce2d05e42ae252dd1d8e490b587c0
yaman1337 <yaman.sarabariya@gmail.com>
2024-03-30 01:26:40 +0545

    refactor github oauth code

--- apps/contributor-web/src/app/page.tsx
+++ apps/contributor-web/src/app/page.tsx
@@ -7,16 +7,11 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const contributors = await db.userContributor.findMany();
   console.log(contributors);
-
-  const session = await getServerSession(options);
-  console.log({ session });
-  if (session) {
-    redirect("/profile");
-  } else {
+  
     return (
       <div>
-        <SignInButton />
+        Welcome To Github Auto Solana Dispenser.
       </div>
     );
-  }
+  
 }
