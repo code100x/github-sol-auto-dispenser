c1f234048fc94a98b08ab82c85820ee823dfda8e
devsargam <sargampoudel100@gmail.com>
2024-04-02 04:56:11 +0530

    feat: setup help page

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './hero';
 export * from './nav-bar';
 export * from './theme-toggler';
-export * from './setup';
+export * from './demo';
 export * from './gradient-bg';
+export * from './setup';
