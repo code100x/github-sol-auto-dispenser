7174d70a5fc554e42d71cc11c9feabdd6cb3721d
devsargam <sargampoudel100@gmail.com>
2024-04-06 06:40:40 +0530

    feat: setup repo viewer page

--- apps/web/src/app/[id]/page.tsx
+++ apps/web/src/app/[id]/page.tsx
@@ -1,6 +1,16 @@
 import { authOptions } from '@/lib/auth';
 import prisma from '@repo/database/client';
 import { getServerSession } from 'next-auth';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 export default async function RepoIdPage(props: {
   params: {
@@ -24,9 +34,63 @@ export default async function RepoIdPage(props: {
     },
   });
 
-  if (!repo) return <NotFound />;
+  const bounties = await prisma.bountyTable.findMany({
+    where: {
+      repoId: Number(id),
+    },
+  });
 
-  return <></>;
+  if (!repo) return <NotFound />;
+  let total = 0;
+  for (const bounty of bounties) {
+    total += bounty.amount;
+  }
+
+  return (
+    <>
+      <h1 className="lg:text-5xl font-bold text-center text-4xl py-5">
+        List of all bounties
+      </h1>
+
+      {bounties.length ? (
+        <div className="px-4 text-xs lg:text-base py-2 rounded-full bg-secondary text-primary max-w-md mx-auto">
+          You haven't given out any bounties for this repo
+        </div>
+      ) : (
+        <section className="mx-auto max-w-lg">
+          <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Amount</TableHead>
+                <TableHead className="text-right">Created At</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {bounties.map((bounty) => (
+                <TableRow key={bounty.id}>
+                  <TableCell className="font-medium">{bounty.id}</TableCell>
+                  <TableCell>{'PAID'}</TableCell>
+                  <TableCell className="text-right">{bounty.amount}</TableCell>
+                  <TableCell className="text-right">
+                    {bounty.createdAt.toDateString()}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total</TableCell>
+                <TableCell className="text-right">${total}</TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </section>
+      )}
+    </>
+  );
 }
 
 function NotFound() {
